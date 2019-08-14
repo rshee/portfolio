3d6752df0d7271efd3aac233256b495f7ddd953b
RAJ <34564999+rshee@users.noreply.github.com>
2019-08-15 08:18:17 +1000

    home page

--- Components/landingpage.js
+++ Components/landingpage.js
@@ -8,7 +8,6 @@ class Landing extends Component {
         <Grid className="landing-grid">
           <Cell col={12}>
             
-
               <div className="banner-text">
                 <h1> Hello world, </h1> 
                 <h2> I am  <span style={{textDecoration: 'none', color: 'rgb(117, 5, 38)', fontFamily: 'Tawakkal Sans' }}><b>Raj Shee.</b></span></h2>
@@ -22,23 +21,19 @@ class Landing extends Component {
                 <a href="https://www.facebook.com/raj.hshee" rel="noopener norefereer" target="_blank">
                   <i class="fa fa-facebook-square" aria-hidden="true"></i>
                 </a>
-
                 {/* LinkedIn */}
                 <a href="https://au.linkedin.com/in/raj-hritisha-shee" rel="noopener norefereer" target="_blank">
                   <i class="fa fa-linkedin-square" aria-hidden="true"></i>
                 </a>
-
                 {/* GitHub */}
                 <a href="https://github.com/rshee" rel="noopener noreferer" target="_blank">
                   <i class="fa fa-github-square" aria-hidden="true"></i>
                 </a>
-
                 {/* Twitter */}
                 <a href="https://twitter.com/rajhshee" rel="noopener norefereer" target="_blank">
                   <i class="fa fa-twitter-square" aria-hidden="true"></i>
                 </a>
-
-              </div>
+                </div>
               </div>
 
               <div className="copyright">
